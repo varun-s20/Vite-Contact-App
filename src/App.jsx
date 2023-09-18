553498e75543a62cfebed30930e6f79be0655462
varun-s20 <varun.jcfanx@gmail.com>
2023-09-18 13:41:33 +0530

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div>
-      App
+    <div className="mx-auto max-w-[500px]">
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[60px] bg-white m-4 rounded-xl flex items-center justify-center text-xl font-serif'>
+        <img src="/vite.svg" />
+        <h1><b>Vite Contact App</b></h1>
+    </div>
+  )
+}
+
+export default Navbar
