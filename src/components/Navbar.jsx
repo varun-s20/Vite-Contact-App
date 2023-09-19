93849a151b8bc27a630fbf2f9ccccf6e632bd097
varun-s20 <varun.jcfanx@gmail.com>
2023-09-19 16:05:57 +0530

    Navbar alignment changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-[60px] bg-white m-4 rounded-xl flex items-center justify-center text-xl font-serif'>
+    <div className='my-4 h-[60px] bg-white m-4 rounded-xl flex items-center justify-center text-xl font-serif'>
         <img src="/vite.svg" />
         <h1><b>Vite Contact App</b></h1>
     </div>
