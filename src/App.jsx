88f8482c1119442a0030d616fcd1cb4a750c5c0c
varun-s20 <varun.jcfanx@gmail.com>
2023-09-19 16:06:33 +0530

    Created search bar and add contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Navbar from "./components/Navbar"
+import {FcSearch} from "react-icons/fc"
+import {BsFillPersonPlusFill} from "react-icons/bs"
 
 const App = () => {
   return (
-    <div className="mx-auto max-w-[500px]">
+    <div className="mx-auto max-w-[500px] px-4">
       <Navbar />
+      <div className="flex gap-3">
+        <div className="relative flex items-center flex-grow">
+          <FcSearch className="ml-2 text-white text-2xl absolute "/>
+          <input type="text" className="h-8 rounded-lg border border-white bg-transparent flex-grow pl-10 text-white" />
+        </div>
+        <BsFillPersonPlusFill className="text-white text-2xl cursor-pointer my-2"/>
+      </div>
     </div>
   )
 }
